b13a55a454a8245f71030276af7b5897dbee9de4
SaiSreenivasReddy <118433103+SaiSreenivasReddy@users.noreply.github.com>
2026-03-17 09:58:24 +0100

    Configuring Biostat

---
+++ Biostat/test/fr/epita/biostat/tests/TestFileInput.java
@@ -0,0 +1,62 @@
+package fr.epita.biostat.tests;
+
+import fr.epita.biostat.datamodel.BioStatEntry;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.OptionalDouble;
+import java.util.Scanner;
+
+public class TestFileInput {
+
+
+    public static void main(String[] args) throws IOException {
+        File file = new File("/Users/saisreenivas/Downloads/JAVA Class/first/Biostat/biostat.csv");
+        System.out.println(file.exists());
+
+
+        Path path = Path.of("/Users/saisreenivas/Downloads/JAVA Class/first/Biostat/biostat.csv");
+        List<String> lines = Files.readAllLines(path);
+        List<BioStatEntry> entries = new ArrayList<>();
+        Scanner scanner = new Scanner(file);
+        scanner.nextLine();
+        while(scanner.hasNext()){
+            String line = scanner.nextLine();
+            System.out.println(line);
+            String[] parts = line.split(",");
+            entries.add(new BioStatEntry(
+                    parts[0].trim(),
+                    parts[1].trim(),
+                    Integer.parseInt(parts[2].trim()),
+                    Integer.parseInt(parts[3].trim()),
+                    Integer.parseInt(parts[4].trim())
+            ));
+        }
+        System.out.println("loaded " + entries.size() + " entries");
+
+        //do the average age
+        double totalAge = 0;
+        for (BioStatEntry entry: entries){
+            totalAge = totalAge + entry.getAge();
+        }
+        double averageAge = totalAge/entries.size();
+        System.out.println(averageAge);
+
+        //do the average age with stream approach
+        OptionalDouble average = entries
+                .parallelStream()
+                .mapToInt(BioStatEntry::getAge)
+                .average();
+
+        if (average.isPresent()){
+            System.out.println(average.getAsDouble());
+        }
+
+
+    }
+}
